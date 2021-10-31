cc177d3a54588b2d485cbf9bd6b88b73be8e06a6
Ilyas Akin <ashnwor@gmail.com>
2021-10-31 21:32:02 +0300

    Add prettier and eslint support

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -1,7 +1,30 @@
-require('null-ls').config {
+local null_ls = require 'null-ls'
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+
+vim.env.PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath 'config'
+  .. '/lua/config/.prettierrc.json'
+
+null_ls.config {
   -- you must define at least one source for the plugin to work
   sources = {
-    require('null-ls').builtins.formatting.stylua.with {
+    diagnostics.eslint.with {
+      command = 'eslint_d',
+    },
+    formatting.prettierd.with {
+      filetypes = {
+        'typescriptreact',
+        'typescript',
+        'javascriptreact',
+        'javascript',
+        'svelte',
+        'json',
+        'jsonc',
+        'css',
+        'html',
+      },
+    },
+    formatting.stylua.with {
       args = {
         '--config-path',
         vim.fn.stdpath 'config' .. '/lua/config/stylua.toml',
