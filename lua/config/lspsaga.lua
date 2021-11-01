e563cf68038a496485543228b0a6833c638b20a8
Ilyas Akin <ashnwor@gmail.com>
2021-11-02 00:17:38 +0300

    Improve lspsaga and lspinstaller

--- lua/config/lspsaga.lua
+++ lua/config/lspsaga.lua
@@ -1,31 +1,47 @@
 local nnoremap = vim.keymap.nnoremap
 
+local provider_module = require 'lspsaga.provider'
+local code_action_module = require 'lspsaga.codeaction'
+local hover_module = require 'lspsaga.hover'
+local action_module = require 'lspsaga.action'
+local signature_help_module = require 'lspsaga.signaturehelp'
+
 nnoremap {
   'gh',
-  require('lspsaga.provider').lsp_finder,
+  provider_module.lsp_finder,
   { silent = true },
 }
 
 nnoremap {
   '<leader>ca',
-  require('lspsaga.codeaction').code_action,
+  code_action_module.code_action,
   { silent = true },
 }
 
 nnoremap {
   'K',
-  require('lspsaga.hover').render_hover_doc,
+  hover_module.render_hover_doc,
   { silent = true },
 }
 
 nnoremap {
   '<C-f>',
-  function() require('lspsaga.action').smart_scroll_with_saga(1) end,
+  function()
+    action_module.smart_scroll_with_saga(1)
+  end,
   { silent = true },
 }
 
 nnoremap {
   '<C-b>',
-  function() require('lspsaga.action').smart_scroll_with_saga(-1) end,
+  function()
+    action_module.smart_scroll_with_saga(-1)
+  end,
+  { silent = true },
+}
+
+nnoremap {
+  'gs',
+  signature_help_module.signature_help,
   { silent = true },
 }
