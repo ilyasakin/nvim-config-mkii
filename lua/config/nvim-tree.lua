226b4a528e9ed2dee9002af4389f7d9083e58be2
Ilyas Akin <ashnwor@gmail.com>
2021-11-29 23:14:16 +0300

    Automatically refresh tree every 1000 ms

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,3 +1,17 @@
 vim.cmd [[
 nnoremap <silent> <C-b> :NvimTreeToggle<CR>
 ]]
+
+local timer = vim.loop.new_timer()
+local refresh_tree = require('nvim-tree.lib').refresh_tree
+
+require('nvim-tree.events').on_nvim_tree_ready(function()
+  timer:start(
+    1000,
+    1000,
+    vim.schedule_wrap(function()
+      refresh_tree()
+    end)
+  )
+end)
+
