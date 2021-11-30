ce02b08c5c787e2076e738aeb53d3a65c58d45d1
Ilyas Akin <ashnwor@gmail.com>
2021-12-01 00:06:40 +0300

    Remove indent lines from packer screens

--- lua/config/indentline.lua
+++ lua/config/indentline.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.post = function()
-  vim.g.indent_blankline_filetype_exclude = { 'dashboard' }
+  vim.g.indent_blankline_filetype_exclude = { 'packer', 'dashboard' }
 end
 
 return M
