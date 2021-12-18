62f9511f10355c95cd66dae71e86dbb257281e92
Ilyas Akin <ashnwor@gmail.com>
2021-12-19 01:23:21 +0300

    Revamp dashboard

--- lua/config/dashboard-nvim.lua
+++ lua/config/dashboard-nvim.lua
@@ -1,45 +1,42 @@
 local M = {}
 
 M.post = function()
-  -- TODO: Modernize
-  vim.cmd [[
-let g:dashboard_default_executive ='telescope'
-let g:dashboard_custom_header =<< trim END
-    ___..._           _...___
-   /'--.._ `'-="""=-'` _..--'\
-   |   ~. )  _     _  ( .~   |
-    \  '~/   a  _  a   \~'  /
-     \  `|     / \     |`  /
-      `'--\    \_/    /--'`
-          .'._  J__.-'.
-         / /  '-/_ `-  \
-        / -"-'-.  '-.__/
-        \__,-.\/     | `\
-        /  ;---.  .--'   |
-       |     /\'-'      /
-       '.___.\   _.--;'`)
-              '-'     `"
-END
+  local nnoremap = vim.keymap.nnoremap
 
-nmap <Leader>ss :<C-u>SessionSave<CR>
-nmap <Leader>sl :<C-u>SessionLoad<CR>
-nnoremap <silent> <Leader>fh :DashboardFindHistory<CR>
-nnoremap <silent> <Leader>ff :DashboardFindFile<CR>
-nnoremap <silent> <Leader>ec :DashboardChangeColorscheme<CR>
-nnoremap <silent> <Leader>fa :DashboardFindWord<CR>
-nnoremap <silent> <Leader>fb :DashboardJumpMark<CR>
-nnoremap <silent> <Leader>fn :DashboardNewFile<CR>
+  vim.g.dashboard_default_executive = 'telescope'
+  vim.g.dashboard_custom_header = {
+    [[    ___..._           _...___]],
+    [[   /'--.._ `'-="""=-'` _..--'\]],
+    [[   |   ~. )  _     _  ( .~   |]],
+    [[    \  '~/   a  _  a   \~'  /]],
+    [[     \  `|     / \     |`  /]],
+    [[      `'--\    \_/    /--'`]],
+    [[          .'._  J__.-'.]],
+    [[         / /  '-/_ `-  \]],
+    [[        / -"-'-.  '-.__/]],
+    [[        \__,-.\/     | `\]],
+    [[        /  ;---.  .--'   |]],
+    [[       |     /\'-'      /]],
+    [[       '.___.\   _.--;'`)]],
+    [[              '-'     `"]],
+  }
 
-let g:dashboard_custom_shortcut={
-\ 'last_session'       : 'SPC s l',
-\ 'find_history'       : 'SPC f h',
-\ 'find_file'          : 'SPC f f',
-\ 'new_file'           : 'SPC f n',
-\ 'change_colorscheme' : 'SPC e c',
-\ 'find_word'          : 'SPC f a',
-\ 'book_marks'         : 'SPC f b',
-\ }
-]]
+  vim.g.dashboard_custom_section = {
+    a = {
+      description = { '  New File                       SPC f n' },
+      command = 'DashboardNewFile',
+    },
+    b = {
+      description = { '  Find File                      SPC f f' },
+      command = 'Telescope find_files',
+    },
+  }
+
+  nnoremap {
+    '<Leader>fn',
+    '<cmd>DashboardNewFile<CR>',
+    { silent = true },
+  }
 end
 
 return M
